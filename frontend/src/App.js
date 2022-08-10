0ac627bb7eaccd83222dd25a0b335b19a0dcd0ca
MDBcode <benkovic.md@gmail.com>
2022-08-10 14:38:10 +0200

    add react functional components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const Header = () => {
+    return (
+        <h1> Docs recommendation system </h1>
+    );
+}
+
+async function fetchRecommendations() {
+const res = await fetch("/languages");
+res
+  .json()
+  .then((res) => setLanguages(res.languages))
+  .catch((err) => setErrors(err));
+}
+
+
+
+const Form = () => {
+   const [data, setData] = useState([])
+
+    useEffect(() => {
+       fetch("/recommendations").then(res => res.json()).then(
+          data => {
+              setData(data)
+              console.log(data)
+          }
+       )
+       }, []
+    )
+
+    return (
+        <>
+            <div className="form-div">
+                <div>
+                    <label for="url">URL:</label>
+                    <input type="text" name="input-url" />
+                </div>
+                <br />
+                <button>Go!</button>
+            </div>
+        </>
+    );
+}
+
+const Visualisation = () => {
+    return (
+        <h1>Here goes D3.</h1>
+    );
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="flexbox-container">
+          <Form />
+          <Visualisation />
+      </div>
     </div>
   );
 }
